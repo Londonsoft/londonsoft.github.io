7318245cfdbf3de111322e51788a41372f00bc97
Londonsoft <blake.lmj@gmail.com>
2026-05-18 17:04:46 -0400

    Simplifies hamburger icon animation

--- build/js/main.js
+++ build/js/main.js
@@ -3,10 +3,10 @@ const initApp = () => {
     const mobileMenu = document.getElementById('mobile-menu')
 
     const toggleMenu = () => {
-        mobileMenu.classList.toggle('hidden')
-        mobileMenu.classList.toggle('flex')
-        hamburgerBtn.classList.toggle('toggle-btn')
-    }
+        mobileMenu.classList.toggle('hidden');
+        mobileMenu.classList.toggle('flex');
+        hamburgerBtn.textContent = mobileMenu.classList.contains('flex') ? '×' : '☰';
+    };
 
     hamburgerBtn.addEventListener('click', toggleMenu)
     mobileMenu.addEventListener('click', toggleMenu)
